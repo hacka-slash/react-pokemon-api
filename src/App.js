62c1c03111490d77295e2d12bf0bc674ccdc01de
hacka-slash <placido.hoff@gmail.com>
2020-04-12 12:30:28 -0400

    React-Pokemon-Api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import Pagination from './Pagination';
+import axios from 'axios';
 
 function App() {
+  const [ pokemon, setPokemon ] =  useState([]);
+  const [ currentPageUrl, setCurrentPageUrl ] = useState("https://pokeapi.co/api/v2/pokemon/")
+  const [ previousPageUrl, setPreviousPageUrl ] = useState()
+  const [ nextPageUrl, setNextPageUrl ] = useState()
+  const [ loading, setLoading ] = useState(true);
+
+  //This 'useEffect()' says everytime the 'currentPageUrl is changed, do the following code. This makes it so that
+   //the entire page does not have to reload. It just reacts with this effect on the 'currentPageUrl' is changed.
+  useEffect(() => {
+    //Each time we make a request, setLoading to true:
+    setLoading(true);
+    let cancel;
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      //Complete the request, setLoading to false
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPreviousPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name)); 
+  })
+    //This logic deletes all previous requests whenever a request is made to eliminate stack error or having old request finish after new requests.
+    //return () => cancel()
+  }, [currentPageUrl])
+
+  function goToNextPage(){
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function goToPreviousPage(){
+    setCurrentPageUrl(previousPageUrl);
+  }
+
+  if(loading) return "Loading..."
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <PokemonList pokemon={pokemon} />
+    <Pagination
+      goToNextPage={nextPageUrl ? goToNextPage : null }
+      goToPrevPage={previousPageUrl ? goToPreviousPage : null} 
+    />
+    </>
   );
 }
 
 export default App;
+ 
